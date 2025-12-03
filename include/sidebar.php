0be23cf42fdb3f61277f5efcafc2525fd2738202
Abdurrahman Alhassan <abdurrahmanalhassan775@gmail.com>
2025-12-03 17:14:41 +0100

    Audit fixes: sessions, includes, login, dashboard; extract sidebar

---
+++ include/sidebar.php
@@ -0,0 +1,8 @@
+<div class="left-sidenav">
+    <div class="brand">
+        <?php require('template/brand_admin.php'); ?>
+    </div>
+    <div class="menu-content h-100" data-simplebar>
+        <?php require('include/menus.php'); ?>
+    </div>
+</div>
